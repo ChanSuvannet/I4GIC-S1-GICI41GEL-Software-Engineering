950b09c4f5ad7aa8e7c80b31d46022b5871848b9
CHAN Suvannet <chansuvannet999@gmail.com>
2024-11-18 16:23:32 +0700

    tp05

--- src/main/java/spring/test/spring_demo/SpringDemoApplication.java
+++ src/main/java/spring/test/spring_demo/SpringDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package spring.test.spring_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
 }
--- src/test/java/spring/test/spring_demo/SpringDemoApplicationTests.java
+++ src/test/java/spring/test/spring_demo/SpringDemoApplicationTests.java
@@ -1,13 +1,14 @@
-package com.example.demo;
+package spring.test.spring_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
